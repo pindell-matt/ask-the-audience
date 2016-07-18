45107d350e7861aba101fbd12032a3db44d586b3
Matt Pindell <pindell.m@gmail.com>
2016-07-17 22:01:14 -0600

    Rudimentary current tally is visible once a vote is submitted

--- public/client.js
+++ public/client.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 var connectionCount = document.getElementById('connection-count');
+var currentTally = document.getElementById('current-tally');
 
 socket.on('usersConnected', function (count) {
   connectionCount.innerText = 'Connected Users: ' + count;
@@ -13,6 +14,7 @@ socket.on('statusMessage', function (message) {
 });
 
 socket.on('voteCount', function (votes) {
+  currentTally.innerText = 'Current Tally: ' + formatVotes(votes);
   console.log(votes);
 });
 
@@ -22,4 +24,13 @@ for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
     socket.send('voteCast', this.innerText);
   });
+}
+
+// do a for each alt option
+function formatVotes(votes){
+  let a = "A: " + votes.A + " ",
+      b = "B: " + votes.B + " ",
+      c = "C: " + votes.C + " ",
+      d = "D: " + votes.D + " ";
+  return a + b + c + d;
 }
